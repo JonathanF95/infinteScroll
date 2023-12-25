ebd069d481c09c444cfcdb277cab74b915ca4dba
Jonathan Fuentes <john.fuen95@gmail.com>
2023-12-25 17:33:35 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
+//Check if all images were loaded
+function imageLoaded(){
+    imagesLoaded++;
+    if(imagesLoaded === totalImages){
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
 //Unsplash API
-const count = 10;
-const apiKey = 'Z0NqKoYY5fKQrVS6FBBViDknsRV4tPqL6VavU1hMg3c';
+const count = 30;
+const apiKey = 'HNSJdZRH89Ze6MEWxLPhVvmQr7ecdxqz7hEuUu7RcPc';
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 //Helper Function to Set Attributes on DOM Elements
@@ -17,6 +29,8 @@ function setAttributes(element, attributes){
 
 //Create Elements for Links and Photos, Add to DOM
 function displayPhotos(){
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     // Run function for each object in photosArray
     photosArray.forEach(photo => {
         //Create <a> to link to Unsplash
@@ -31,7 +45,9 @@ function displayPhotos(){
             src: photo.urls.regular,
             alt: photo.alt_description,
             title: photo.alt_description,
-        })
+        });
+        //Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded)
         //Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -48,6 +64,14 @@ async function getPhotos(){
         //Catch Error Here
     }
 }
+
+//Check to see if scrolling near bottom of page, Load more photos
+window.addEventListener('scroll', () =>{
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
+        getPhotos()
+    }
+})
 
 //On Load
 getPhotos()
